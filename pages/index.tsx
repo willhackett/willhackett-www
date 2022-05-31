e6aec8e0e5b63136d74ac981a00faabf767d2a6c
Will Hackett <12025460+willhackett@users.noreply.github.com>
2022-05-31 21:20:08 +1000

    fix: 🐛 remove content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,4 @@
 import Head from 'next/head';
-import { Fragment } from 'react';
-
-const links: NodeJS.Dict<[string, string | null, string][]> = {
-  Present: [
-    ['AutoGrab', null, 'https://www.autograb.com.au/'],
-    ['venueScout GmbH', null, 'https://www.venuescout.org/'],
-  ],
-  Past: [
-    ['Coles Group', '2020', 'https://www.colesgroup.com.au/'],
-    ['StartOut Australia', '2019', 'https://www.startout.org.au/'],
-    ['SEEK Limited', '2019', 'https://www.startout.org.au/'],
-    ['Localz', '2018', 'https://www.localz.com/'],
-    ['InnoWell @ PwC', '2018', 'https://www.innowell.org/'],
-    ['Enablo', '2017', 'https://www.enablo.com/'],
-    ['OpenClub', '2017', 'https://www.openclub.com.au/'],
-    ['Word of House', '2016', 'https://www.willhackett.com/'],
-    ['Expedia', '2014', 'https://www.expedia.com/'],
-  ],
-  Links: [
-    [
-      'GPG Key',
-      null,
-      'https://keys.openpgp.org/vks/v1/by-fingerprint/087CC35930841DB15E4C9A87DD2A653B8B6E9865',
-    ],
-    ['LinkedIn', null, 'https://www.willhackett.com/in/willhackett/'],
-    ['GitHub', null, 'https://www.github.com/willhackett'],
-  ],
-};
 
 export default function Index() {
   return (
@@ -71,23 +43,10 @@ export default function Index() {
           <h1 className="text-2xl uppercase tracking-wider font-extralight">
             Will Hackett
           </h1>
-          <h2 className="my-4 lg:my-8">Leave it better than you found it.</h2>
+          <h2 className="my-4 lg:my-8">
+            Leave things better than you found them.
+          </h2>
         </header>
-        {Object.keys(links).map((key) => (
-          <section key={key} className="mb-4 lg:mb-8">
-            <h3 className="text-xs uppercase tracking-wider">{key}</h3>
-            <ul className="list-disc list-outside pl-4">
-              {links[key]?.map(([name, small, href]) => (
-                <li key={href}>
-                  <a href={href} className="hover:underline">
-                    {name}
-                    {small && <small>, {small}</small>}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </section>
-        ))}
         <main></main>
         <footer className="text-xs uppercase">:)</footer>
       </div>
